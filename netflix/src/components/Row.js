df0e38abbbe143dd1814145cd2f34dc9a80b0828
Gavin1103 <gtjin1103@gmail.com>
2022-02-25 15:28:04 +0100

    begin netflix

---
+++ netflix/src/components/Row.js
@@ -0,0 +1,25 @@
+// import { createElement } from "react";
+
+var RowContainer = document.getElementsByClassName('row');
+for (let row = 0; row < 11; row++) {
+    
+    console.log(row);
+    // var rowDiv = createElement('div');
+    // rowDiv.className("divRow");
+    
+    
+}
+
+
+function Row() {
+    return (
+        <div className="row">
+
+
+
+
+        </div>
+    );
+}
+
+export default Row;
