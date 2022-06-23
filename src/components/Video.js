6b79f86138a91dbea201bb29be2edd4609947c26
Gavin <gtjin1103@gmail.com>
2022-06-23 12:58:02 +0100

    online zetten op heroku

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,7 +4,7 @@ function Video() {
     return (
         <video id="playFilm" controls>
             {/* <source src={video} type="video/mp4" /> */}
-            {/* <source src="http://u533187.gluweb.nl/videos/video.html" type="video/mp4" /> */}
+            <source src="http://u533187.gluweb.nl/videos/video.html" type="video/mp4" />
         </video>
     );
 }
